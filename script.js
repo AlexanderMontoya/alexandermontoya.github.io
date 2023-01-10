3f73cd40e0c937d74c0ea779caecd4d667b61be1
unknown <montinformatic233@gmail.com>
2023-01-10 18:08:59 -0500

    Dando estilos y funciones al cont_div

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log("Hola Mundo");
+
+if(location.href=="https://alexandermontoya.github.io/PrimerProyecto/#HolMundo"){
+    document.getElementById('cont_div').style.backgroundColor="blue";
+}
